68b53bc26b568d911cd48c964c194fb7ee4b0a18
Takeshi KOMIYA <i.tkomiya@gmail.com>
2018-04-12 23:18:46 +0900

    Update requirements to Sphinx >= 1.6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 long_desc = open('README.rst').read()
 
-requires = ['Sphinx>=0.6', 'sphinxcontrib-imagehelper>=1.1.0']
+requires = ['Sphinx>=1.6', 'sphinxcontrib-imagehelper>=1.1.0']
 
 setup(
     name='sphinxcontrib-cacoo',
@@ -25,10 +25,10 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Documentation',
         'Topic :: Documentation :: Sphinx',
         'Topic :: Utilities',
